e948f9868974cdd240a4d6d335723023616c75e6
Gabriel Richter <gabrielrichter@migrate.info>
2021-04-12 13:59:30 -0300

    Update

--- files/find_mdf_ldf_files.sql
+++
@@ -1,6 +0,0 @@
-DECLARE @DBName AS VARCHAR(50)
-SET @DBName = 'TreinamentoDBA'
-
-SELECT name, physical_name 
-FROM sys.master_files 
-WHERE database_id = DB_ID(@DBName);
--- files/modify_mdf_ldf_files.sql
+++
@@ -1,5 +0,0 @@
--- *.mdf
-ALTER DATABASE TreinamentoDBA MODIFY FILE ( NAME = TreinamentoDBA, FILENAME = 'C:\TEMP\TreinamentoDBA.mdf')
-
--- *.ldf
-ALTER DATABASE TreinamentoDBA MODIFY FILE ( NAME = TreinamentoDBA_log, FILENAME = 'C:\TEMP\TreinamentoDBA_log.ldf')
