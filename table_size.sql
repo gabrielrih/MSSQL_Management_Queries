8d38badd7dbe1c2b7682a93ba88602f1e504b2d0
Gabriel Richter <gabriel.richter@ambevtech.com.br>
2022-10-06 15:17:26 -0300

    Add query to get table size

--- table_size.sql
+++ table_size.sql
@@ -1,6 +1,21 @@
-USE Traces
-GO
-
+-- Query 1
+SELECT
+    t.NAME AS TableName,
+    SUM(a.total_pages) * 8 AS TotalSpaceKB, 
+    SUM(a.used_pages) * 8 AS UsedSpaceKB, 
+    (SUM(a.total_pages) - SUM(a.used_pages)) * 8 AS UnusedSpaceKB
+FROM
+    sys.tables t
+INNER JOIN
+    sys.partitions p ON t.object_id = p.OBJECT_ID
+INNER JOIN
+    sys.allocation_units a ON p.partition_id = a.container_id
+GROUP BY
+    t.Name
+ORDER BY
+    TotalSpaceKB Desc
+    
+-- Query 2
 SELECT
     t.NAME AS Entidade,
     p.rows AS Registros,
