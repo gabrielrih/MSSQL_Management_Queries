f7db83c053899880ce9022ce4ba78c2794ef5fe0
Gabriel Richter <gabrielrih@gmail.com>
2024-12-11 10:56:28 -0300

    Update check_open_conn.sql

--- connections/check_open_conn.sql
+++ connections/check_open_conn.sql
@@ -16,3 +16,17 @@ INNER JOIN sys.databases b ON a.dbid = b.database_id
 GROUP BY a.dbid, b.name, a.hostname
 ORDER BY TotalConnections DESC
 GO
+
+-- Agrupar por usuário
+SELECT 
+    login_name AS UserName,
+    COUNT(session_id) AS TotalConnections
+FROM 
+    sys.dm_exec_sessions
+WHERE 
+    is_user_process = 1 -- Filters out system sessions
+GROUP BY 
+    login_name
+ORDER BY 
+    login_name
+GO
