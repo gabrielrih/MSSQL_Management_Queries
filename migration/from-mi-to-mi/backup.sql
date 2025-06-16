f49827830fc040b47e28af7cbd5cb7c00036a7b3
Gabriel Richter <gabrielrih@gmail.com>
2025-06-16 20:23:24 -0300

    Create backup.sql

---
+++ migration/from-mi-to-mi/backup.sql
@@ -0,0 +1,24 @@
+-- Criar credencial 
+IF NOT EXISTS (
+	SELECT *
+	   FROM sys.credentials
+	   WHERE name = 'https://my.blob.core.windows.net/dump'
+)
+	CREATE CREDENTIAL [https://my.blob.core.windows.net/dump]
+	WITH IDENTITY = 'SHARED ACCESS SIGNATURE', SECRET = 'secret-here'
+	GO
+
+-- Backup para Storage Account
+-- References:
+--	https://learn.microsoft.com/en-us/sql/relational-databases/backup-restore/sql-server-backup-to-url?view=sql-server-ver17#limitations-of-backuprestore-to-azure-blob-storage
+--	https://learn.microsoft.com/en-us/sql/t-sql/statements/backup-transact-sql?view=sql-server-ver17
+-- Recomendo rodar via job para evitar de cair a sessão do usuário
+BACKUP DATABASE [database-name]
+TO
+URL = 'https://my.blob.core.windows.net/dump/hbnfe_prod_1.bak',
+URL = 'https://my.blob.core.windows.net/dump/hbnfe_prod_2.bak',
+URL = 'https://my.blob.core.windows.net/dump/hbnfe_prod_3.bak',
+URL = 'https://my.blob.core.windows.net/dump/hbnfe_prod_4.bak',
+URL = 'https://my.blob.core.windows.net/dump/hbnfe_prod_5.bak'
+WITH COPY_ONLY, INIT, COMPRESSION, STATS = 5
+GO
